8f1e8ebe7c35df8e54b088dcb57d714f9cddd4a6
thomas <thomas.mathers@outlook.com>
2020-05-15 01:04:30 +1200

    set up project

---
+++ navapp/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import { View, Text, FlatList, StyleSheet, Alert, Button } from 'react-native'
+import Expo from 'expo'
+import { StackNavigator } from 'react-navigation'
+
+
+
+
+export default class App extends Component {
+  render(){
+    return (
+      <View>
+
+        <Text> hello </Text>
+
+      </View>
+     )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+  }
+})
